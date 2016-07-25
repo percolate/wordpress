acf9e1e987c841331c6ffcd241f4793bcac075ac
Balazs Szabo <balazs@eegl.co>
2016-07-25 22:04:25 +0200

    logging: proper checking if log file is writable

--- models/percolate-log.php
+++ models/percolate-log.php
@@ -42,11 +42,16 @@ class Percolate_Log
   public static function log($msg='')
   {
     $uploads = wp_upload_dir();
+    $dir = $uploads['basedir'] . DIRECTORY_SEPARATOR . self::LOGS_DIRECTORY;
 
-    if ( is_dir($uploads['basedir'] . DIRECTORY_SEPARATOR . self::LOGS_DIRECTORY) and is_writable($uploads['basedir'] . DIRECTORY_SEPARATOR . self::LOGS_DIRECTORY))
+    if ( is_dir($dir) && is_writable($dir) && is_writable($dir . DIRECTORY_SEPARATOR . self::LOG_FILE))
     {
-      $date = date('d.m.Y h:i:s');
-      error_log($date . " | " . $msg . "\n", 3, $uploads['basedir'] . DIRECTORY_SEPARATOR . self::LOGS_DIRECTORY . DIRECTORY_SEPARATOR . self::LOG_FILE);
+      try {
+        $date = date('d.m.Y h:i:s');
+        error_log($date . " | " . $msg . "\n", 3, $dir . DIRECTORY_SEPARATOR . self::LOG_FILE);
+      } catch (Exception $e) {
+        error_log($msg);
+      }
     } else {
       error_log($msg);
     }
@@ -55,11 +60,12 @@ class Percolate_Log
   public static function getLog()
   {
     $uploads = wp_upload_dir();
-    $file = file_get_contents( $uploads['basedir'] . DIRECTORY_SEPARATOR . self::LOGS_DIRECTORY . DIRECTORY_SEPARATOR . self::LOG_FILE );
-    if( empty($file) ) {
-      $res = array("success" => false);
-    } else {
+    $furi = $uploads['basedir'] . DIRECTORY_SEPARATOR . self::LOGS_DIRECTORY . DIRECTORY_SEPARATOR . self::LOG_FILE;
+    if (  is_readable($furi)) {
+      $file = file_get_contents( $furi );
       $res = array("success" => true, "log" => $file);
+    } else {
+      $res = array("success" => false);
     }
     return $res;
   }
