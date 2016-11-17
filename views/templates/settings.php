55955e6062322196fc7f9a234307cdc762b9191d
Balazs Szabo <balazs@eegl.co>
2016-11-17 22:17:56 +0100

    percolate test

--- views/templates/settings.php
+++ views/templates/settings.php
@@ -79,5 +79,5 @@
 <hr>
 
 <p>
-  <small>Plugin version: 4.x-1.1.2 (<i>Supported WordPress version - Plugin version</i>)</small>
+  <small>Plugin version: 4.x-1.1.3 (<i>Supported WordPress version - Plugin version</i>)</small>
 </p>
